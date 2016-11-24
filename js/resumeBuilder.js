55bff0f8e0216a5a89fda94b699411fac43dba08
isagiongo <isagiongo@yahoo.com.br>
2016-11-24 10:28:23 -0200

    Formatação do header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,19 @@
-$("#main").append(["Isa Giongo"]);
+//$("#main").append(["Isa Giongo"]);
+
+var nome = "Isadora Giongo";
+console.log(nome);
+
+var email = "isagiongo@hotmail.com";
+
+var newEmail = email.replace("hotmail","gmail");
+
+console.log(email);
+console.log(newEmail);
+//$("#main").append([email]);
+//$("#main").append([newEmail]);
+var funcao = "Analista de Testes"
+var nomeFormatado = HTMLheaderName.replace("%data%", nome);
+var funcaoFormatada = HTMLheaderRole.replace("%data%",funcao);
+
+$("#header").append(nomeFormatado);
+$("#header").append(funcaoFormatada);
